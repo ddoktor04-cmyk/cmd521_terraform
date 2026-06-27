d63103412ea86de106dc0d25f3c7e4cddf6a5070
macnaer <trofimchuk.an@gmail.com>
2026-06-27 15:48:50 +0300

    Added variables

--- vars.tf
+++ vars.tf
@@ -7,3 +7,28 @@ variable "aws_secret_key" {
   type    = string
   default = ""
 }
+
+variable "aws_region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "aws_zone" {
+  type    = string
+  default = "eu-north-1a"
+}
+
+variable "aws_image_id" {
+  type    = string
+  default = "ami-0aba19e56f3eaec05"
+}
+
+variable "aws_instance_type" {
+  type    = string
+  default = "t3.small"
+}
+
+variable "aws_key_name" {
+  type    = string
+  default = "Stockholm_3"
+}
